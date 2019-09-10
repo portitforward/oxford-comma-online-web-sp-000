8365326d333c7af5e3456e4b9d830e6f9643c0b1
Cody Fussell <cody.fussell@gmail.com>
2019-09-10 23:44:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(list)
+    if list.length == 1
+      return list.join()
+    elsif list.length == 2
+      return list.join(" and ")
+    else
+      str = list[0, list.length()-1].join(", ")
+      str += ", and #{list[-1]}"
+      return str
+    end  
 end
