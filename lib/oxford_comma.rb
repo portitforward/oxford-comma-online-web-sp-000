0fef590108d427f4cfad21839d34a1a667bd4d99
Cody Fussell <cody.fussell@gmail.com>
2019-09-10 23:37:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
-def oxford_comma(array)
-  new_string array[:-2].join(", ")
+def oxford_comma(list)
+    str = list[0, list.length()-1].join(", ")
+    str += " and #{list[-1]}."
+    return str
 end
